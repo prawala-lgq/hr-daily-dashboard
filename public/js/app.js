5e42acb5064a15cbf914c2936b43bc803f9f17be
prawala-lgq <prawala@logique.co.id>
2026-03-25 11:25:20 +0700

    Create app.js

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+// ============================================================
+// HRFlow — APP.JS
+// Entry point — jalankan app saat halaman load
+// ============================================================
+
+document.addEventListener('DOMContentLoaded', function() {
+  updateBadge();
+  loadFromDB();
+});
